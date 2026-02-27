d8be7ad7ae223d61af70850c8f8aa09068627093
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-02-27 23:49:55 +0800

    feat: add app strings for authentication and role selection screens

--- lib/resources/app_strings.dart
+++ lib/resources/app_strings.dart
@@ -1,4 +1,38 @@
 class AppStrings {
   static String appName = "RideGuide";
   static String subName = "Your Journey, Our Guide";
+  static String realTime = "Real-Time Tracking";
+  static String realTimeDesc = "Track your ride in real time,\nno guesswork needed.";
+  static String fareCalculator = "Fare Calculator";
+  static String fareCalculatorDesc = "Get accurate fare estimates\nbefore you ride.";
+  static String searchLocation = "Search Location";
+  static String searchLocationDesc = "Find the nearest terminal\nwith just a few taps.";
+  static String signUp = "Join Now";
+  static String login = "Login";
+
+  // Sign Up Screen
+  static String signUpTitle = "Sign Up";
+  static String name = "Name";
+  static String email = "Email";
+  static String password = "Password";
+  static String termsPrefix = "By signing up, you agree to the ";
+  static String termsLink = "Terms of Service and Privacy Policy";
+  static String orWith = "Or with";
+  static String signUpWithGoogle = "Sign Up with Google";
+  static String signUpWithFacebook = "Sign Up with Facebook";
+  static String alreadyHaveAccount = "Already have an account? ";
+
+  // Login Screen
+  static String loginTitle = "Login";
+  static String forgotPassword = "Forgot Password?";
+  static String dontHaveAccount = "Don't have an account yet? ";
+
+  // Choose Role Screen
+  static String chooseRole = "Choose Your Role";
+  static String chooseRoleDesc = "Select how you'd like to use RideGuide";
+  static String driver = "Driver";
+  static String driverDesc = "Offer rides and earn money\nby driving passengers.";
+  static String commuter = "Commuter";
+  static String commuterDesc = "Find and book rides to your\ndestination with ease.";
+  static String continueText = "Continue";
 }
--- lib/resources/app_styles.dart
+++ lib/resources/app_styles.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppStyles {
+  static const String _fontFamily = 'Inter';
+
   static TextStyle titleX({double size = 40, Color color = Colors.black}) {
     return TextStyle(
+      fontFamily: _fontFamily,
       fontSize: size,
       fontWeight: FontWeight.bold,
       color: color,
@@ -11,6 +14,7 @@ class AppStyles {
 
   static TextStyle subText({double size = 40, Color color = Colors.black}) {
     return TextStyle(
+      fontFamily: _fontFamily,
       fontSize: size,
       fontWeight: FontWeight.normal,
       color: color,
