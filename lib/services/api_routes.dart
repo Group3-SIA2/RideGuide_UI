c1ea5982294ba23f6caee01f9dc9b7f873de13a9
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-03-01 01:22:19 +0800

    feat: add ApiRoutes class for managing API endpoint URLs

---
+++ lib/services/api_routes.dart
@@ -0,0 +1,8 @@
+class ApiRoutes {
+  static String baseUrl = 'http://192.168.1.6:8000/api';
+  static String registerUrl = '$baseUrl/auth/register';
+  static String loginUrl = '$baseUrl/auth/login';
+  
+
+
+}
