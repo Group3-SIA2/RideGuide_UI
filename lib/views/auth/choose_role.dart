d52222be7581e9639146d6f1b80b3835ad3d53ad
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-03-01 02:41:28 +0800

    refactor: disable back button in choose role screen

--- lib/views/auth/choose_role.dart
+++ lib/views/auth/choose_role.dart
@@ -21,10 +21,7 @@ class _ChooseRoleScreenState extends State<ChooseRoleScreen> {
         backgroundColor: Colors.white,
         elevation: 0,
         scrolledUnderElevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.pop(context),
-        ),
+        automaticallyImplyLeading: false,
         centerTitle: true,
         title: Text(
           AppStrings.chooseRole,
